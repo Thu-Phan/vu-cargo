2ccfa6b4d3ff41f3a5e76e4d3edf45ada3437232
Phan Huỳnh Anh Thư <phath@netcompany.com>
2024-11-04 20:49:36 +0700

    m

--- src/whyChooseUs.tsx
+++ src/whyChooseUs.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import helpIcon from "./assets/help.png";
+import financialIcon from "./assets/financial-report.png";
+import profitIcon from "./assets/profit.png";
 
 const FeatureCard = ({ icon, title, description }) => {
   return (
     <div className="flex flex-col items-center text-center">
       <div className="w-16 h-16 mb-4 p-2 bg-white rounded-lg shadow-lg">
         <img src={icon} alt={title} className="mb-4 w-12 h-12" />
-
-        {/* {icon} */}
       </div>
       <h3 className="text-red-500 font-medium mb-2">{title}</h3>
       <p className="text-gray-600 text-sm hidden md:block">{description}</p>
@@ -17,20 +18,19 @@ const FeatureCard = ({ icon, title, description }) => {
 const WhyChooseUs = () => {
   const features = [
     {
-      icon: "src\\assets\\help.png",
+      icon: helpIcon,
       title: "Uy Tín",
       description:
         "Với 8 năm kinh nghiệm trong lĩnh vực xuất nhập khẩu cùng với sự tin tưởng của hơn 10.000 khách hàng, Vu Cargo tự tin mang dịch vụ của mình ra khắp nhiều nước.",
     },
-
     {
-      icon: "src\\assets\\help.png",
+      icon: financialIcon,
       title: "An Toàn",
       description:
         "Mọi đơn hàng đều được Vu Cargo đóng pallet cẩn thận. Khách hàng có thể gửi mọi loại hàng từ giá trị cao, hàng dễ vỡ cho đến hàng nhạy cảm cần đảm bảo chắc chắn.",
     },
     {
-      icon: "src\\assets\\help.png",
+      icon: profitIcon,
       title: "Tiết Kiệm",
       description:
         "Với hình thức vận chuyển trọn gói, Vu Cargo luôn dành ưu đãi cho những khách hàng lớn để giúp tiết kiệm một khoản đáng kể cước phí vận chuyển",
